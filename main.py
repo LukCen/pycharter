dcdf2d26caa0b79a8536e69a98e2c5b1e32006e0
lbjcena <lbjcena@gmail.com>
2025-10-08 21:37:48 +0200

    line chart replaced with bars

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ class DataRow():
     # ogranizational container
     self.frame = ttk.Frame(master)
 
+
+    # labels for inputs plus inputs
+
     self.new_label_x = ttk.Label(self.frame, text="X axis (string)")
     self.new_label_x.configure(padding=10)
 
@@ -24,16 +27,17 @@ class DataRow():
     self.new_entry_x = ttk.Entry(self.frame, width=60)
     self.new_entry_y = ttk.Entry(self.frame, width=60)
 
+    # add to list button
 
-    self.new_button = ttk.Button(self.frame,text="Add to list", command=self.add_to_list)
+    self.new_button = ttk.Button(self.frame, text="Add to list", command=self.add_to_list)
 
-    self.new_entry_x.grid(row=0,column=1, sticky='ew')
+    self.new_entry_x.grid(row=0, column=1, sticky='ew')
     self.new_entry_y.grid(row=1, column=1, sticky='ew')
 
-    self.new_label_x.grid(row=0,column=0)
+    self.new_label_x.grid(row=0, column=0)
     self.new_label_y.grid(row=1, column=0)
 
-    self.new_button.grid(row=0, column=2, sticky='e')
+    self.new_button.grid(row=0, column=2, sticky='nse')
 
     self.new_button.rowconfigure(0, weight=2)
 
@@ -45,18 +49,31 @@ class DataRow():
     text_x = self.new_entry_x.get()
     text_y = self.new_entry_y.get()
     if text_x and text_y:
-      text_list.insert('', 'end',None, text=f'{text_x } : { text_y}')
+      # text_list.insert('', 'end',None, text=f'{text_x } : { text_y}')
       items.update({text_x: text_y})
+
+      list_item_frame = ttk.Frame(self.frame)
+
+      list_item_key = ttk.Label(list_item_frame, text=text_x)
+      list_item_value = ttk.Label(list_item_frame, text=text_y)
+
+      list_item_key.pack(anchor="w")
+      list_item_value.pack(anchor="e")
+      list_item_frame.grid(row=2,column=0, sticky='ew')
+      list_item_frame.rowconfigure(2, weight=2)
 
         
 # key/value pairs to use for the chart making
 items = {}
-names = [items.keys()]
-values = [items.values()]
+
+def plotLineChart(title = "My Chart"):
+    
+    sorted_items = sorted(items.items(), key=lambda x: int(x[1]))
+    names = [item[0] for item in sorted_items]
+    values = [int(item[1]) for item in sorted_items]
 
 
-def plotLineChart(axis_x, axis_y, title = "My Chart"):
-    mpl.plot(axis_x, axis_y)
+    mpl.bar(names, values)
     mpl.title(title)
     mpl.show()
 
@@ -77,11 +94,11 @@ frame1.grid(row=0,column=0,sticky="nsew")
 frame1.columnconfigure(0, weight=1)
 
 # textlist - displays added k/v pairs
-text_list = ttk.Treeview(root)
-text_list.grid(rows=1,column=0,sticky='nsew')
+# text_list = ttk.Treeview(root)
+# text_list.grid(rows=1,column=0,sticky='nsew')
 
 
-chart_button = tkinter.Button(root,text="Chart", command=lambda : plotLineChart(*names, *values), background="#333333", foreground="#f5f5f5", font="25", padx=20, pady=10)
+chart_button = tkinter.Button(root,text="Chart", command=plotLineChart, background="#333333", foreground="#f5f5f5", font="25", padx=20, pady=10)
 chart_button.grid(row=3, columnspan=3, sticky='nsew')
 x_row = DataRow(frame1,0,0)
 
